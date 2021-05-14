01cc879122c7deb0b8b1f7529239511743631874
sobaotto <you@example.com>
2021-05-14 12:41:21 +0900

    github actions CD

--- src/component/body/exam/PhotosOfWord.tsx
+++ src/component/body/exam/PhotosOfWord.tsx
@@ -1,11 +1,11 @@
-import { Image, Flex } from "@chakra-ui/react";
+import { Image, Flex, Box } from "@chakra-ui/react";
 
 export const PhotosOfWord = (): JSX.Element => {
   const currentQuestion = 1;
   const photoSrc: Array<string> = [
     "https://images.unsplash.com/photo-1515073838964-4d4d56a58b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1515073838964-4d4d56a58b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1515073838964-4d4d56a58b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    // "https://images.unsplash.com/photo-1515073838964-4d4d56a58b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    // "https://images.unsplash.com/photo-1515073838964-4d4d56a58b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
     // "https://images.unsplash.com/photo-1515073838964-4d4d56a58b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
   ];
 
@@ -16,6 +16,8 @@ export const PhotosOfWord = (): JSX.Element => {
       justifyContent="space-between"
       alignItems="center"
       backgroundColor="gray.100"
+      overflow="hidden"
+      dir="ltr"
     >
       {photoSrc.map((src, index) => {
         return (
@@ -23,8 +25,9 @@ export const PhotosOfWord = (): JSX.Element => {
             key={index}
             src={src}
             alt="top-image"
-            width="60%"
+            width="80%"
             margin="0 20px"
+            scroll-snap-type=" x mandatory"
           ></Image>
         );
       })}
